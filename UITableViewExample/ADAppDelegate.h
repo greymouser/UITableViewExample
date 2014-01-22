2e7a76d4989df3f4e6f0d88897b051df28f25da3
Armando Di Cianno <armando@dicianno.org>
2014-01-22 08:20:41 -0500

    Initial Commit

---
+++ UITableViewExample/ADAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  ADAppDelegate.h
+//  UITableViewExample
+//
+//  Created by Armando Di Cianno on 1/22/14.
+//  Copyright (c) 2014 Armando Di Cianno. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ADAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ UITableViewExample/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  UITableViewExample
+//
+//  Created by Armando Di Cianno on 1/22/14.
+//  Copyright (c) 2014 Armando Di Cianno. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "ADAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([ADAppDelegate class]));
+    }
+}
